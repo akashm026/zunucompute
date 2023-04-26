6696e4ed383e042f7a8cccacd7e698d667cca3b7
akashm026 <76767089+akashm026@users.noreply.github.com>
2023-04-26 10:32:03 +0530

    changing the execution handler interface input

--- programmerparadigm/src/main/java/com/ziroh/handler/ExecuteHandler.java
+++ programmerparadigm/src/main/java/com/ziroh/handler/ExecuteHandler.java
@@ -4,5 +4,5 @@ import com.ziroh.compute.pojo.Input;
 import com.ziroh.compute.pojo.Output;
 
 public interface ExecuteHandler {
-    Output execute(Input... inputs);
+    Output execute(Input inputs);
 }
