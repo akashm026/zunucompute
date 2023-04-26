f102f979263e3e5d32de2072049610f27483aa43
samarjeet <samarjeet.sahoo@ziroh.com>
2023-04-26 20:50:31 +0530

    oooo

--- processor/src/main/java/com/ziroh/compute/handler/ExecutionHandler.java
+++ processor/src/main/java/com/ziroh/compute/handler/ExecutionHandler.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.Arrays;
 
 public class ExecutionHandler {
     public Output execute(Node node, Input input) {
@@ -22,6 +23,7 @@ public class ExecutionHandler {
             }, ExecuteHandler.class.getClassLoader());
             var calculatorClass = Class.forName("com.usecase.Calculator", true, classLoader); // We need to pass this calculator
             final Class<?> loadedClass = Class.forName(lambdaFunction, true, classLoader);
+            getDependentClass(loadedClass);
             for (var method : loadedClass.getDeclaredMethods()) {
                 output = (Output) method.invoke(loadedClass.getDeclaredConstructor(calculatorClass).newInstance(calculatorClass.newInstance()), input);
             }
@@ -33,4 +35,21 @@ public class ExecutionHandler {
         return output;
     }
 
+    /**
+     * @param inputClass the class whose object we want to create
+     * @return class that is reqd. to create object of inputClass
+     */
+    private Class<?> getDependentClass(Class<?> inputClass) {
+        var constructors = inputClass.getDeclaredConstructors();
+        System.out.println("Total number of constructors available:" + constructors.length);
+        Arrays.stream(constructors)
+                .forEach(constructor -> {
+                    Arrays.stream(constructor.getParameterTypes()).forEach(aClass -> {
+                        System.out.println(aClass.getName());
+                    });
+                });
+        return inputClass;
+    }
+
+    /* TODO: What if the executor has multiple constructors and how do we know which one the use to create object for the executor ? */
 }
